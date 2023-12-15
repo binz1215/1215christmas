1e8815fccf1e57371053b171a620e446f7ee31c1
binz1215 <binz1215@naver.com>
2023-12-15 14:38:33 +0900

    constants setting

--- src/constants/contants.js
+++ src/constants/contants.js
@@ -1,5 +1,32 @@
-export const X = [];
+export const DAY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-export const Y = {
-  key: value,
+export const STAR_DAY = [3, 10, 17, 24, 25, 31];
+
+export const DISCOUNT = {
+  START: 1000,
+  PLUS: 100,
+
+  WEEK: 2023,
+
+  STAR: 5000,
+  TREE: 10000,
+  SANTA: 20000,
+
+  CHAMPAGNE: 120000,
 };
+export const MENU = [
+  { menu: '양송이수프', category: '애피타이저', price: 6000 },
+  { menu: '타파스', category: '애피타이저', price: 5500 },
+  { menu: '시저샐러드', category: '애피타이저', price: 8000 },
+  { menu: '티본스테이크', category: '메인', price: 55000 },
+  { menu: '바비큐립', category: '메인', price: 54000 },
+  { menu: '해산물파스타', category: '메인', price: 35000 },
+  { menu: '크리스마스파스타', category: '메인', price: 25000 },
+  { menu: '초코케이크', category: '디저트', price: 15000 },
+  { menu: '아이스크림', category: '디저트', price: 5000 },
+  { menu: '제로콜라', category: '음료', price: 3000 },
+  { menu: '레드와인', category: '음료', price: 60000 },
+  { menu: '샴페인', category: '음료', price: 25000 },
+];
+
+export const CHAMPAGNE = '샴페인';
