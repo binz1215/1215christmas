076777c398214822caad274a661b208ffa85cde8
binz1215 <binz1215@naver.com>
2023-12-15 14:45:18 +0900

    입력 파일 작성

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -1,7 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
 
-export async function input() {
-  const XYZ = await Console.readLineAsync('\n');
+export async function inputDay() {
+  const day = await Console.readLineAsync(
+    '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
+  );
 
-  return XYZ;
+  return day;
+}
+
+export async function inputMenu() {
+  const menu = await Console.readLineAsync('주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n');
+
+  return menu;
 }
